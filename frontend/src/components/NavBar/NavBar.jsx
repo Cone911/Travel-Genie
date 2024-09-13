91ee0017ea74a462e8a694cfb966cfcd4830d74d
JP_Escobari <jpe911@gmail.com>
2024-09-13 15:30:23 -0400

    Add icons, fonts and styling for NavBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,9 @@
+import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+
 import * as authService from '../../services/authService';
 import './NavBar.css';
 
@@ -10,10 +15,9 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav className="NavBar">
-      <Link to="/">Home</Link>
-      &nbsp; | &nbsp;
+      <div className='logo'><Link to="/"><h1>Travel Genie</h1></Link></div>
       {user ? (
-        <>
+        <div className='navigation'>
           <Link to="/posts">Post List</Link>
           &nbsp; | &nbsp;
           <Link to="/posts/new">New Post</Link>
@@ -23,13 +27,13 @@ export default function NavBar({ user, setUser }) {
           </Link>
           &nbsp;&nbsp;
           <span>Welcome, {user.name}</span>
-        </>
+        </div>
       ) : (
-        <>
-          <Link to="/login">Log In</Link>
+        <div className='navigation'>
+          <Link to="/login"><FontAwesomeIcon className='icon' icon={faRightFromBracket} size="lg" style={{color: "goldenrod"}} /> Log In</Link>
           &nbsp; | &nbsp;
-          <Link to="/signup">Sign Up</Link>
-        </>
+          <Link to="/signup"><FontAwesomeIcon className='icon' icon={faUserPlus} size="lg" style={{color: "goldenrod"}} /> Sign Up</Link>
+        </div>
       )}
     </nav>
   );
