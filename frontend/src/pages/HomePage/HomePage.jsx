25827f01e927aa4e4a4ac2c49d32854698e88ca8
jp-escobari <jpe911@gmail.com>
2024-09-14 20:06:11 -0400

    Add Popular Destinations image carousel

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import PopularDestinations from '../../components/PopularDestinations/PopularDestinations';
 import Sidebar from '../../components/Sidebar/Sidebar';
-import '../HomePage/HomePage.css'
+import '../HomePage/HomePage.css';
 
 export default function HomePage({ toggleSidebar, isSidebarVisible }) {
   return (
