885d93bae32e013e4e8757cc3c03b07aa7527400
JP_Escobari <jpe911@gmail.com>
2024-09-15 21:44:28 -0400

    Add trip duration selector bubble box

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import PlacesAutocomplete from 'react-places-autocomplete';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
 import './SearchBar.css';
 
 export default function SearchBar() {
 
   const [destination, setDestination] = useState('');
+  const [days, setDays] = useState(1);
+  const [showDaysSelector, setShowDaysSelector] = useState(true);
 
   const handleChange = (value) => {
     setDestination(value);
@@ -15,8 +19,22 @@ export default function SearchBar() {
     console.log('Selected destination:', destination); // TODO: handle the selected destination.
   };
 
+  function incrementDays() {
+    if (days < 14) setDays(days + 1);
+  }
+
+  function decrementDays() {
+    if (days > 1) setDays(days -1);
+  }
+
+  function toggleDaysSelector () {
+    setShowDaysSelector(!showDaysSelector);
+  }
+
   return (
     <div className="search-bar">
+
+      {/* STEP 1: Select a Destination */}
       <PlacesAutocomplete
         value={destination}
         onChange={handleChange}
@@ -32,7 +50,7 @@ export default function SearchBar() {
               })}
             />
             <div className="autocomplete-dropdown">
-              {loading && <div>Loading...</div>}
+              {loading && <div><FontAwesomeIcon icon={faWandMagicSparkles} shake /></div>}
               {suggestions.map((suggestion) => {
                 const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
                 const style = { backgroundColor: suggestion.active ? '#fafafa' : '#ffffff', cursor: 'pointer' };
@@ -51,7 +69,27 @@ export default function SearchBar() {
           </div>
         )}
       </PlacesAutocomplete>
-      <input type="text" placeholder="Trip Duration" className="search-input" />
+      
+      {/* STEP 2: Select the trip duration. */}
+      <div className="trip-duration">
+          <input
+            type="text"
+            placeholder="Trip Duration"
+            className="search-input"
+            value={days > 0 ? `${days} day${days > 1 ? 's' : ''}` : ''}
+            onClick={toggleDaysSelector}
+            readOnly
+          />
+          {showDaysSelector && (
+          <div className={`days-selector`}>
+            <button className="circle-button" onClick={decrementDays} disabled={days <= 1}>-</button>
+            <span>{days} day{days > 1 ? 's' : ''}</span>
+            <button className="circle-button" onClick={incrementDays} disabled={days >= 14}>+</button>
+          </div>
+        )}
+      </div>
+
+      {/* STEP 3: Specify the size of your party. */}
       <input type="text" placeholder="Party Size" className="search-input" />
       <button className="search-button">Ask the Genie!</button>
     </div>
