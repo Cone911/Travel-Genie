b28a87bbf6bd93ae5d0c6c797b18e1c054f03e51
JP_Escobari <jpe911@gmail.com>
2024-09-20 02:11:34 -0400

    Style Log In Page

--- frontend/src/pages/LogInPage/LogInPage.jsx
+++ frontend/src/pages/LogInPage/LogInPage.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import '../LogInPage/LogInPage.css'
 import * as authService from '../../services/authService';
 
 export default function LogInPage({ setUser }) {
@@ -24,28 +28,31 @@ export default function LogInPage({ setUser }) {
   }
 
   return (
-    <>
-      <h2>Log In!</h2>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
+    <div className='main-log-in-container'>
+      <h2>Log In</h2>
+      <form id='log-in-form' autoComplete="off" onSubmit={handleSubmit}>
+      <fieldset>
+      <legend><em>YOUR CREDENTIALS:</em></legend>
+        <div class="inputBox">
+          <input type="email" name="email" value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <label>Email</label>
+        </div>
+
+          <div className='inputBox'>
+            <input type="password" name="password"value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <label>Password</label>
+          </div>
+        </fieldset>
         <button type="submit">LOG IN</button>
+      <Link style={{textAlign: "center"}} to="/signup">Not Registered? &nbsp; <FontAwesomeIcon className='icon' icon={faUserPlus} size="lg" style={{color: "goldenrod"}} /> Sign Up</Link>
       </form>
       <p className="error-message">&nbsp;{errorMsg}</p>
-    </>
+    </div>
   );
 }
