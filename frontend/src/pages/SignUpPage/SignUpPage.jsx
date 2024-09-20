128b291b2c8650e20f6e7bab7c32662f199770b5
JP_Escobari <jpe911@gmail.com>
2024-09-20 02:23:47 -0400

    Add Styling for Sign Up Form

--- frontend/src/pages/SignUpPage/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage/SignUpPage.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import '../SignUpPage/SignUpPage.css'
 import * as authService from '../../services/authService';
 
 export default function SignUpPage({ setUser }) {
@@ -30,26 +34,36 @@ export default function SignUpPage({ setUser }) {
   const disable = formData.password !== formData.confirm;
 
   return (
-    <>
+    <div className='main-log-in-container'>
       <h2>Sign Up!</h2>
-      <form autoComplete="off" onSubmit={handleSubmit}>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <label>Password</label>
+      <form autoComplete="off" onSubmit={handleSubmit} id='sign-up-form'>
+      <fieldset>
+      <legend><em>YOUR CREDENTIALS:</em></legend>
+        <div className='inputBox'>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            />
+          <label>Name</label>
+        </div>
+        <div className='inputBox'>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            />
+          <label>Email</label>
+        </div>
+        </fieldset>
+
+        <fieldset>
+        <legend><em>YOUR PASSWORD:</em></legend>
+        <div className='inputBox'>
         <input
           type="password"
           name="password"
@@ -57,7 +71,10 @@ export default function SignUpPage({ setUser }) {
           onChange={handleChange}
           required
         />
-        <label>Confirm</label>
+        <label>Password</label>
+        </div>
+        
+        <div className='inputBox'>
         <input
           type="password"
           name="confirm"
@@ -65,11 +82,15 @@ export default function SignUpPage({ setUser }) {
           onChange={handleChange}
           required
         />
+        <label>Confirm</label>
+        </div>
+        </fieldset>
         <button type="submit" disabled={disable}>
           SIGN UP
         </button>
+        <Link style={{textAlign: "center"}} to="/login">Already registered? &nbsp;<FontAwesomeIcon className='icon' icon={faRightFromBracket} size="lg" style={{color: "goldenrod"}} /> Log In</Link>
       </form>
       <p className="error-message">&nbsp;{errorMsg}</p>
-    </>
+    </div>
   );
 }
